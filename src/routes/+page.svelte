374185fc51ffa71a5c572748ecc50b3e195f3843
TypucT <247582173+TypucT-TTV@users.noreply.github.com>
2026-07-10 16:52:50 +0300

    release: bump version to v2.0.14 and include src-tauri/db.json in Git repository

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -584,7 +584,7 @@
   <aside class="sidebar">
     <div class="logo-container">
       <img src="/logo.png" class="logo-img" alt="logo" />
-      <h2>G-Lock <span class="ver">v2.0.13</span></h2>
+      <h2>G-Lock <span class="ver">v2.0.14</span></h2>
     </div>
 
     <nav class="nav-links">
